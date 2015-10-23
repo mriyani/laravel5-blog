f341b033fbe2e81edd26855cbf2a9d4879b5ad92
riyani makhtar <ryaneq@gmail.com>
2015-10-23 10:04:54 +0800

    registration n login sample

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,29 +1,31 @@
-<!-- resources/views/auth/register.blade.php -->
+@extends('master')
 
+@section('content')
 <form method="POST" action="/auth/register">
     {!! csrf_field() !!}
 
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" value="{{ old('email') }}" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" name="password_confirmation" class="form-control">
     </div>
 
     <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
-
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
-
-    <div>
-        <button type="submit">Register</button>
+        <button type="submit" class="btn btn-primary">Register</button>
     </div>
 </form>
+@stop
